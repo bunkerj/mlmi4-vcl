0ab4b63358b8b9187a0f6d122715ba55df188236
rb876 <rb876@cam.ac.uk>
2019-02-17 19:06:15 +0100

    check loss output

--- neural_trainer.py
+++ neural_trainer.py
@@ -9,18 +9,19 @@ class NeuralTrainer():
     def __init__(self, neuralNetwork):
         # Create random Tensors to hold input and output
         self.neuralNetwork = neuralNetwork
+        # self.train_step = torch.optim.Adam(self.neuralNetwork.parameters(), lr = 0.001)
 
     def _assignOptimizer(self, learning_rate = 0.001):
-        self.train_step = torch.optim.Adam(model.parameters(), lr = learning_rate)
+        self.train_step = torch.optim.Adam(self.neuralNetwork.parameters(), lr = learning_rate)
 
-    def train(self, x_train, y_train, no_epochs=1000, batch_size=100, display_epoch=5):
+    def train(self, x_train, y_train, task_id = 0, no_epochs=1000, batch_size=100, display_epoch=5):
         N = x_train.shape[0]
         if batch_size > N:
             batch_size = N
         # training cycle
-        cost = [];
+        costs = [];
         for epoch in range(no_epochs):
-            perm_inds = range(x_train.shape[0])
+            perm_inds = list(range(x_train.shape[0]))
             np.random.shuffle(perm_inds)
             cur_x_train = x_train[perm_inds]
             cur_y_train = y_train[perm_inds]
@@ -33,16 +34,15 @@ class NeuralTrainer():
                 batch_x = cur_x_train[start_ind:end_ind, :]
                 batch_y = cur_y_train[start_ind:end_ind, :]
                 # Run optimization op (backprop) and cost op (to get loss value)
-                self.x = batch_x
-                self.y = batch_y
-                y_pred = self.neuralNetwork.model(x) # MODEL NOT DEFINED YET
-                loss = self.neuralNetwork.loss(y_pred, y) # THIS IS NOT DEFINED YET
-                train_step.zero_grad()
+                y_pred = self.neuralNetwork(batch_x)
+                loss = self.neuralNetwork.loss(y_pred, batch_y)
+                self._assignOptimizer()
+                self.train_step.zero_grad()
                 loss.backward()
-                train_step.step()
+                self.train_step.step()
                 c = loss.data.item()
                 # Compute average loss
-                avg_cost += c / total_bat
+                avg_cost += c / total_batch
             if epoch % display_epoch == 0:
                 print("Epoch:", '%04d' % (epoch+1), "cost=", \
                     "{:.9f}".format(avg_cost))
@@ -51,5 +51,5 @@ class NeuralTrainer():
         return costs
 
     def getWeights(self):
-        weights = self.neuralNetwork.model.parameters()
+        weights = self.neuralNetwork.parameters()
         return weights
