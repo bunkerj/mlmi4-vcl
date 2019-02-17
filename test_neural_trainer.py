d335f1f40f5455c9b64fd765991c3b9a3f5d1ccb
rb876 <rb876@cam.ac.uk>
2019-02-17 20:26:16 +0100

    epochs number changed and no external batchsize

--- test_neural_trainer.py
+++ test_neural_trainer.py
@@ -13,8 +13,7 @@ device = torch.device('cuda')
 # Hyperparameters
 input_size = 784
 hidden_size = 500
-num_classes = 10
-num_epochs = 1
+num_classes = 5
 batch_size = 100
 learning_rate = 0.001
 
@@ -29,10 +28,10 @@ test_dataset = torchvision.datasets.MNIST(root = '../../data',
 
 # Data Loader
 train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
-                                            batch_size = batch_size,
+                                            batch_size = 600000,
                                             shuffle = True)
 test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
-                                            batch_size = batch_size,
+                                            batch_size = 600000,
                                             shuffle = False)
 
 def _onehot(labels):
@@ -44,10 +43,22 @@ def _onehot(labels):
 
 for i, (images, labels) in enumerate(train_loader):
         # Move tensors to the configured device
-        print(images.size())
         images = images.reshape(-1, 28*28).to(device)
         y_onehot = _onehot(labels)
         testNN = TestNN(input_size, hidden_size, num_classes)
         neuralTrainer = NeuralTrainer(testNN)
-        neuralTrainer.train(images, y_onehot, no_epochs = 100, batch_size = 25)
+        neuralTrainer.train(images, y_onehot, no_epochs = 40, batch_size = 200, display_epoch = 20)
         break;
+
+with torch.no_grad():
+    correct = 0;
+    total = 0;
+
+    for images, labels in test_loader:
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+        outputs = testNN(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+    print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
