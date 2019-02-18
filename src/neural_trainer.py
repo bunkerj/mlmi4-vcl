47b191d8a7591d2a4d68ae0ce6a13c4cc9378d12
rb876 <rb876@cam.ac.uk>
2019-02-19 00:31:45 +0100

    change variable names

--- src/neural_trainer.py
+++ src/neural_trainer.py
@@ -9,40 +9,40 @@ class NeuralTrainer():
         # Create random Tensors to hold input and output
         self.neuralNetwork = neuralNetwork
 
-    def _assignOptimizer(self, learning_rate = 0.001):
-        self.train_step = torch.optim.Adam(self.neuralNetwork.parameters(), lr = learning_rate)
+    def _assignOptimizer(self, learningRate = 0.001):
+        self.train_step = torch.optim.Adam(self.neuralNetwork.parameters(), lr = learningRate)
 
-    def train(self, x_train, y_train, task_id = 0, no_epochs=1000, batch_size=100, display_epoch=5):
-        N = x_train.shape[0]
-        if batch_size > N:
-            batch_size = N
+    def train(self, xTrain, yTrain, taskId = 0, noEpochs=1000, batchSize=100, displayEpoch=5):
+        N = xTrain.shape[0]
+        if batchSize > N:
+            batchSize = N
         # Training cycle
         costs = [];
-        for epoch in range(no_epochs):
-            perm_inds = list(range(x_train.shape[0]))
-            np.random.shuffle(perm_inds)
-            cur_x_train = x_train[perm_inds]
-            cur_y_train = y_train[perm_inds]
+        for epoch in range(noEpochs):
+            permInds = list(range(xTrain.shape[0]))
+            np.random.shuffle(permInds)
+            curxTrain = xTrain[permInds]
+            curyTrain = yTrain[permInds]
 
-            avg_cost = 0.
-            total_batch = int(np.ceil(N * 1.0 / batch_size))
-            for i in range(total_batch):
-                start_ind = i*batch_size
-                end_ind = np.min([(i+1)*batch_size, N])
-                batch_x = cur_x_train[start_ind:end_ind, :]
-                batch_y = cur_y_train[start_ind:end_ind, :]
+            avgCost = 0.
+            totalBatch = int(np.ceil(N * 1.0 / batchSize))
+            for i in range(totalBatch):
+                startInd = i*batchSize
+                endInd = np.min([(i+1)*batchSize, N])
+                batchX = curxTrain[startInd:endInd, :]
+                batchY = curyTrain[startInd:endInd, :]
                 # Run optimization op (backprop) and cost op (to get loss value)
-                y_pred = self.neuralNetwork(batch_x)
-                loss = self.neuralNetwork.loss(y_pred, batch_y)
+                yPred = self.neuralNetwork(batchX)
+                loss = self.neuralNetwork.loss(yPred, batchY)
                 self._assignOptimizer()
                 self.train_step.zero_grad()
                 loss.backward()
                 self.train_step.step()
                 # Compute average loss
-                avg_cost += loss / total_batch
-                if (i+1) % display_epoch == 0:
+                avgCost += loss / totalBatch
+                if (i+1) % displayEpoch == 0:
                     print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                        .format(epoch+1, no_epochs, i+1, total_batch, avg_cost))
-            costs.append(avg_cost)
+                        .format(epoch+1, noEpochs, i+1, totalBatch, avgCost))
+            costs.append(avgCost)
         print("Optimization Finished!")
         return costs
