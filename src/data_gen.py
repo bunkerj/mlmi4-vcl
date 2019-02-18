4830cb8f79e67fb019e72e5ec1f9b61744ad6f3f
Gwangbin Bae <baegwangbin@gmail.com>
2019-02-18 18:26:04 +0000

    add parent class to load mnist data

--- src/data_gen.py
+++ src/data_gen.py
@@ -10,42 +10,44 @@ def onehot(labels, num_classes):
     y_onehot = torch.from_numpy(y_onehot)#.to(device)
     return y_onehot
 
-class MnistGen():
+# Mnist Data Loader
+class Mnist()
     def __init__(self):
         # Define number of tasks
-        self.maxIter = 1
-        self.curIter = 1
-        self.X_train = torch.load('../data/MNIST_X_train.pt')
+        self.X_train = torch.load('../data/MNIST_X_train.pt').reshape(-1, 28*28)
         self.Y_train = torch.load('../data/MNIST_Y_train.pt')
-        self.X_test = torch.load('../data/MNIST_X_test.pt')
+        self.X_test = torch.load('../data/MNIST_X_test.pt').reshape(-1, 28*28)
         self.Y_test = torch.load('../data/MNIST_Y_test.pt')
 
+# Mnist Generator (no split or permutation)
+class MnistGen(Mnist):
+    def __init__(self):
+        super().__init__()
+        self.maxIter = 1
+        self.curIter = 1
+
     def get_dims(self):
-        return 784, 1
+        return self.X_train.shape[1], 1
 
     def next_task(self):
         if self.curIter > self.maxIter:
             raise Exception('Task finished!')
         else:
-            self.curIter += 1
-
             next_x_train = self.X_train
             next_y_train = onehot(self.Y_train, 10)
-
             next_x_test = self.X_test
             nex_y_test = onehot(self.Y_test, 10)
 
+            self.curIter += 1
+
             return nex_x_train, next_y_train, next_x_test, next_y_test
 
-class SplitMnistGen()
+# Split Mnist Generator
+class SplitMnistGen(Mnist):
     def __init__(self):
+        super().__init__()
         self.maxIter = 5
         self.curIter = 1
-        self.X_train = torch.load('../data/MNIST_X_train.pt')
-        self.Y_train = torch.load('../data/MNIST_Y_train.pt')
-        self.X_test = torch.load('../data/MNIST_X_test.pt')
-        self.Y_test = torch.load('../data/MNIST_Y_test.pt')
-
         self.sets_0 = [0, 2, 4, 6, 8]
         self.sets_1 = [1, 3, 5, 7, 9]
 
@@ -54,7 +56,7 @@ class SplitMnistGen()
         self.sets_1 = set1
 
     def get_dims(self):
-        return 784, 2
+        return self.X_train.shape[1], 2
 
     def next_task(self):
         if self.curIter > self.maxIter:
@@ -62,15 +64,15 @@ class SplitMnistGen()
         else:
             idx = (self.Y_train == 2)
 
-            train_id_0 = X_train[self.Y_train == self.sets_0[self.curIter], :, :, :]
-            train_id_1 = X_train[self.Y_train == self.sets_1[self.curIter], :, :, :]
+            train_id_0 = self.X_train[self.Y_train == self.sets_0[self.curIter], :]
+            train_id_1 = self.X_train[self.Y_train == self.sets_1[self.curIter], :]
             next_x_train = torch.cat([train_id_0, train_id_1], dim=0)
 
             next_y_train = torch.cat([torch.ones(train_id_0.shape[0], 1),torch.zeros(train_id_1.shape[0], 1)],dim=0)
             next_y_train = torch.cat([next_y_train, 1-next_y_train])
 
-            test_id_0 = X_train[self.Y_test == self.sets_0[self.curIter], :, :, :]
-            test_id_1 = X_train[self.Y_test == self.sets_1[self.curIter], :, :, :]
+            test_id_0 = self.X_train[self.Y_test == self.sets_0[self.curIter], :, :, :]
+            test_id_1 = self.X_train[self.Y_test == self.sets_1[self.curIter], :, :, :]
             next_x_test = torch.cat([test_id_0, test_id_1], dim=0)
 
             next_y_test = torch.cat([torch.ones(test_id_0.shape[0], 1),torch.zeros(test_id_1.shape[0], 1)],dim=0)
