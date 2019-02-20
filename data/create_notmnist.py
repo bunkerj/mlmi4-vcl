a577e0642f8e775621b53b39123537122f0b2da4
Gwangbin Bae <baegwangbin@gmail.com>
2019-02-20 13:31:20 +0000

    add create notmnist

---
+++ data/create_notmnist.py
@@ -0,0 +1,42 @@
+################################################################################
+# Load & Save NotMnist Dataset as Pickle File ##################################
+################################################################################
+
+from scipy.io import loadmat
+import numpy as np
+import torch
+
+# the data is dictionary type
+data = loadmat('notMNIST_small.mat')
+
+# extract images & labels
+# data['images'] has shape of (28, 28, 18724)
+# so it is reshaped properly
+X = data['images'].reshape(28*28, -1).T
+Y = data['labels']
+
+# Transform numpy arrays into tensors
+X = torch.from_numpy(X)
+Y = torch.from_numpy(Y)
+
+# for reproducibility
+torch.manual_seed(0)
+
+# in the data, images of same character are stacked together
+# so we need to shuffle them
+# idx is the shuffled index
+idx = torch.randperm(X.shape[0]) # X.shape[0] is the number of images
+
+# 90% of the data will be used as training data
+n_train = int(X.shape[0]*0.9)
+
+X_train = X[idx[:n_train],:]
+Y_train = Y[idx[:n_train]]
+X_test = X[idx[n_train:],:]
+Y_test = Y[idx[n_train:]]
+
+# save tensors as pickle file
+torch.save(X_train, 'NotMNIST_X_test.pt')
+torch.save(Y_train, 'NotMNIST_Y_test.pt')
+torch.save(X_test, 'NotMNIST_X_test.pt')
+torch.save(Y_test, 'NotMNIST_Y_test.pt')
