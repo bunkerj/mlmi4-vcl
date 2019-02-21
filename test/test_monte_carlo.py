af96db2567539193d3437957f1bb3b1c0419c304
rb876 <rb876@cam.ac.uk>
2019-02-21 15:01:20 +0100

    add tetsing for prediction probabilities

--- test/test_monte_carlo.py
+++ test/test_monte_carlo.py
@@ -12,19 +12,23 @@ from monte_carlo import MonteCarlo
 from constants import FloatTensor, MEAN, VARIANCE, WEIGHT, BIAS
 
 inputSize = 784
-hiddenSize = 10
-numLayers = 3
-numClasses = 10
-batchSize = 200
+hiddenSize = 256
+numLayers = 5
+numClasses = 2
+batchSize = 50
 numSamples = 10
 
 sharedDim = (2, inputSize, hiddenSize)
-headDim = (1, hiddenSize, numClasses)
+headDim = (3, hiddenSize, numClasses)
 headCount = 5
 
 fakeBatch = torch.randn(batchSize,inputSize).type(FloatTensor)
+fakeTarget = torch.cat([torch.ones(25, 2), torch.zeros(25, 2)]).type(FloatTensor)
+
 qPosterior = ParametersDistribution(sharedDim, headDim, headCount)
 vanillaNN = VanillaNN(inputSize, hiddenSize, numLayers, numClasses)
 monteCarlo = MonteCarlo(vanillaNN)
 result = monteCarlo.computeMonteCarlo(fakeBatch, qPosterior, 1, numSamples)
 print(result)
+predictionProb = monteCarlo.logPred(fakeBatch, qPosterior, 1, numSamples, fakeTarget)
+print(predictionProb)
