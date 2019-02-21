3a61a41e35f164c89723db7b027d62e807b21189
rb876 <rb876@cam.ac.uk>
2019-02-21 22:48:12 +0100

    new optimization test bench

--- test/test_optimization.py
+++ test/test_optimization.py
@@ -22,7 +22,7 @@ inputSize = 784
 hiddenSize = 256
 numLayers = 3
 numClasses = 10
-numSamples = 10
+numSamples = 100
 batchSize = 100
 
 sharedDim = (2, inputSize, hiddenSize)
@@ -43,7 +43,7 @@ trainLoader = torch.utils.data.DataLoader(dataset = trainDataset,
                                             batch_size = 600000,
                                             shuffle = True)
 testLoader = torch.utils.data.DataLoader(dataset = testDataset,
-                                            batch_size = 600000,
+                                            batch_size = 100,
                                             shuffle = False)
 
 def _onehot(labels):
@@ -78,9 +78,25 @@ def maximizeVariationalLowerBound(model, x_train, y_train, qPrior, taskId):
             minimizeLoss(1, optimizer, computeCost, lossArgs)
         return qPosterior
 
+qPrior = ParametersDistribution(sharedDim, headDim, headCount)
 for i, (images, labels) in enumerate(trainLoader):
     images = images.reshape(-1, 28*28).to(Device)
     yOnehot = _onehot(labels)
-    qPrior = ParametersDistribution(sharedDim, headDim, headCount)
     model = VanillaNN(inputSize, hiddenSize, numLayers, numClasses)
-    maximizeVariationalLowerBound(model, images, yOnehot, qPrior, taskId = 2)
+    qPosterior = maximizeVariationalLowerBound(model, images, yOnehot, qPrior, taskId = 2)
+    qPrior.overwrite(qPosterior)
+
+with torch.no_grad():
+    correct = 0;
+    total = 0;
+
+    for images, labels in testLoader:
+        images = images.reshape(-1, 28*28).to(Device)
+        labels = labels.to(Device)
+        model = VanillaNN(inputSize, hiddenSize, numLayers, numClasses)
+        monteCarlo = MonteCarlo(model)
+        predicted = monteCarlo.computeMonteCarlo(images, qPrior, 2, numSamples)
+        _, predicted = torch.max(predicted.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+    print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
