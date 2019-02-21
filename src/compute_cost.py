d61bff6cbfdbabf26e48247b890d6641c72e8d68
rb876 <rb876@cam.ac.uk>
2019-02-21 22:03:09 +0100

    new cost function with model as input

--- src/compute_cost.py
+++ src/compute_cost.py
@@ -8,9 +8,9 @@ from constants import FloatTensor, MEAN, VARIANCE, WEIGHT, BIAS
 from monte_carlo import MonteCarlo
 from KL import KL
 
-def computeCost(inputs, labels, qPos, qPri, taskId):
-    monteCarlo = MonteCarlo()
+def computeCost(model, inputs, labels, qPos, qPri, taskId):
+    monteCarlo = MonteCarlo(model)
     kl = KL()
-    cost = monteCarlo.logPred(inputs, labels, qPos, taskId, inputs.size()[0]) - torch.div(kl.computeKL(qPos, qPri, taskId), inputs.size()[0])
+    cost = - (monteCarlo.logPred(inputs, labels, qPos, taskId, inputs.size()[0]) - torch.div(kl.computeKL(qPos, qPri, taskId), 10)) 
 
     return  cost
