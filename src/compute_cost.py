c6522062a091edadb3ffeb4b73d965bed19e904e
rb876 <rb876@cam.ac.uk>
2019-02-22 19:53:39 +0100

    modify #samples

--- src/compute_cost.py
+++ src/compute_cost.py
@@ -11,6 +11,6 @@ from KL import KL
 def computeCost(model, inputs, labels, qPos, qPri, taskId):
     monteCarlo = MonteCarlo(model)
     kl = KL()
-    cost = - (monteCarlo.logPred(inputs, labels, qPos, taskId, inputs.size()[0]) - torch.div(kl.computeKL(qPos, qPri, taskId), 10)) 
+    cost = - (monteCarlo.logPred(inputs, labels, qPos, taskId, inputs.size()[0]) - torch.div(kl.computeKL(qPos, qPri, taskId), inputs.size()[0])) 
 
     return  cost
