1b1e8edce9f1d104cf686c843a6e05ea7c62bdaa
unknown <justin_bunker@hotmail.com>
2019-02-26 15:34:49 +0000

    Update forward pass test

--- test/test_optimization_with_forward_pass.py
+++ test/test_optimization_with_forward_pass.py
@@ -4,11 +4,6 @@ sys.path.append('../src/')
 import torch
 from optimizer import minimizeLoss
 
-def computeCost(testNN, input):
-    cost = testNN(input) ** 2
-    print(cost)
-    return cost
-
 class TestNN(torch.nn.Module):
     def __init__(self):
         super(TestNN, self).__init__()
@@ -21,20 +16,25 @@ class TestNN(torch.nn.Module):
     def getParameters(self):
         return [self.fc1.weight.transpose(0, 1), self.fc1.bias]
 
-    def setParameters(self, weight, bias):
+    def setParameters(self, parameters):
+        # Can anything be done here to stay connected to the graph?
+        weight, bias = parameters
         self.fc1.weight = torch.nn.Parameter(weight.transpose(0, 1))
         self.fc1.bias = torch.nn.Parameter(bias)
 
-input = torch.randn(1,10)
+def computeCost(parameters, input):
+    testNN = TestNN()
+    testNN.setParameters(parameters)
+    cost = testNN(input) ** 2
+    print(cost) # Cost stays the same :(
+    return cost
+
+input = torch.randn(1, 10)
 weight = torch.ones((10, 1))
 bias = torch.ones((1, 1))
-parameters = [weight, bias]
 
-testNN = TestNN()
-testNN.setParameters(weight, bias)
-
-lossArgs = (testNN, input)
-
-print(computeCost(*lossArgs))
+parameters = (weight, bias)
+lossArgs = (parameters, input)
 optimizer = torch.optim.Adam(parameters, lr = 0.01)
+
 minimizeLoss(10, optimizer, computeCost, lossArgs)
