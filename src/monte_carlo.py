bd786e15addd4088c095eac0f1280489a2c3d1b8
rb876 <rb876@cam.ac.uk>
2019-02-27 18:53:32 +0100

    new monte carlo

--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -4,49 +4,65 @@ sys.path.append('../')
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from constants import Device, FloatTensor, MEAN, VARIANCE, WEIGHT, BIAS
 
 class MonteCarlo:
 
-    def __init__(self, neuralNetwork):
-        self.neuralNetwork = neuralNetwork
+    def __init__(self, qPos, numSamples):
+        self.numSamples = numSamples
+        self.qPos = qPos
 
     def _computeParameters(self, m, v, eps):
         return torch.add(torch.mul(eps, v), m)
 
-    def computeMonteCarlo(self, inputs, qPos, taskId, numSamples):
+    def _getParameterDims(self, PARAMETER):
+        return PARAMETER.size()
+
+    def _getSampledParametersDims(self, PARAMETER):
+        return ((self.numSamples, self._getParameterDims(PARAMETER[1])[0], self._getParameterDims(PARAMETER[1])[1]) \
+                    if PARAMETER[0] == 'weight' \
+                        else (self.numSamples, self._getParameterDims(PARAMETER[1])[0]))
+
+    def _createParameterSample(self, funcGetSpecificParameters, PARAMETER, layerId, taskId = None):
+        m = funcGetSpecificParameters(PARAMETER, MEAN, taskId)[layerId]
+        v = funcGetSpecificParameters(PARAMETER, VARIANCE, taskId)[layerId]
+        eps = torch.randn(self._getSampledParametersDims((PARAMETER, m))).type(FloatTensor)
+
+        return self._computeParameters(m, v, eps)
+
+    def _forwardPass(self, inputs, weights, biases):
+        act = inputs
+        numLayers = len(weights)
+        for i in range(numLayers):
+            pred = torch.add(torch.matmul(act, weights[i]), biases[i])
+            act = F.relu(pred)
+        return pred
+
+    def _loss(self, output, labels):
+        loss = torch.sum(- labels * F.log_softmax(output, -1), -1)
+        return loss.mean()
+
+    def computeMonteCarlo(self, inputs, taskId):
 
         weightSample, baisesSample = [], []
-        for layerId, mW in enumerate(qPos.getShared(WEIGHT,MEAN)):
-            vW = qPos.getShared(WEIGHT, VARIANCE)[layerId]
-            mB = qPos.getShared(BIAS, MEAN)[layerId]
-            vB = qPos.getShared(BIAS, VARIANCE)[layerId]
+        for layerId, mW in enumerate(self.qPos.getShared(WEIGHT, MEAN)):
+            weightSample.append(self._createParameterSample(self.qPos.getShared, WEIGHT, layerId))
+            baisesSample.append(self._createParameterSample(self.qPos.getShared, BIAS, layerId))
 
-            epsW = torch.randn((numSamples, mW.size()[0], mW.size()[1])).type(FloatTensor)
-            epsB = torch.randn((numSamples, mB.size()[0])).type(FloatTensor)
-            weightSample.append(self._computeParameters(mW, vW, epsW))
-            baisesSample.append(self._computeParameters(mB, vB, epsB))
-
-        for layerId, mW in enumerate(qPos.getHead(WEIGHT,MEAN,taskId)):
-            vW = qPos.getHead(WEIGHT, VARIANCE, taskId)[layerId]
-            mB = qPos.getHead(BIAS, MEAN, taskId)[layerId]
-            vB = qPos.getHead(BIAS, VARIANCE, taskId)[layerId]
-
-            epsW = torch.randn((numSamples, mW.size()[0], mW.size()[1])).type(FloatTensor)
-            epsB = torch.randn((numSamples, mB.size()[0])).type(FloatTensor)
-            weightSample.append(self._computeParameters(mW, vW, epsW))
-            baisesSample.append(self._computeParameters(mB, vB, epsB))
+        for layerId, mW in enumerate(self.qPos.getHead(WEIGHT, MEAN, taskId)):
+            weightSample.append(self._createParameterSample(self.qPos.getHead, WEIGHT, layerId, taskId))
+            baisesSample.append(self._createParameterSample(self.qPos.getHead, BIAS, layerId, taskId))
 
         pred = torch.zeros((inputs.size()[0], mW.size()[1])).type(FloatTensor)
-        for k in range(numSamples):
+        for k in range(self.numSamples):
             weights = [weight[k, :, :] for weight in weightSample]
             biases = [bias[k, :] for bias in baisesSample]
-            self.neuralNetwork.setParameters((weights, biases))
-            pred += self.neuralNetwork(inputs).to(Device)
+            pred += self._forwardPass(inputs, weights, biases)
 
-        return pred/numSamples
+        return pred/self.numSamples
 
-    def logPred(self, inputs, labels, qPos, taskId, numSamples):
-        pred = self.computeMonteCarlo(inputs, qPos, taskId, numSamples)
-        logLik = self.neuralNetwork.loss(pred, labels)
+    def logPred(self, inputs, labels, taskId):
+        pred = self.computeMonteCarlo(inputs, taskId)
+        logLik = self._loss(pred, labels)
         return logLik
