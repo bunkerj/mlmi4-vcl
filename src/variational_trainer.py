0aea05ba620f0056e84d822bb78cb7a22502bc88
Gwangbin Bae <baegwangbin@gmail.com>
2019-02-27 18:06:22 +0000

    fix error

--- src/variational_trainer.py
+++ src/variational_trainer.py
@@ -85,10 +85,10 @@ class VariationalTrainer:
             if t == 0:
                 model = VanillaNN(self.inputDim, self.hiddenSize, self.numSharedLayers+self.numHeadLayers, self.outputDim)
                 modelTrainer = NeuralTrainer(model)
-                modelTrainer.train(x_train, y_train, self.numEpochs, self.batchSize, displayEpoch = 20)
+                modelTrainer.train(x_train, y_train, None, self.numEpochs, self.batchSize, displayEpoch = 20)
                 param_mean = model.getParameters()
                 # use parameter mean to initialize the q prior
-                self.qPosterior.setParameters(param_mean)
+                self.qPosterior.setParameters(param_mean, headId)
 
             # if coreset size is not zero, create coreset
             if self.coresetSize > 0:
@@ -97,14 +97,14 @@ class VariationalTrainer:
                     x_coresets[taskId], y_coresets[taskId], x_train, y_train = self.coresetMethod(x_train, y_train, self.coresetSize)
 
             # update weights and bias for current task
-            self.qPosterior.overwrite(maximizeVariationalLowerBound(self.qPosterior, x_train, y_train, headId))
+            self.qPosterior.overwrite(self.maximizeVariationalLowerBound(self.qPosterior, x_train, y_train, headId))
 
             # qPred and inference
-            for t_ in range(t):
+            for t_ in range(t+1):
                 taskId_ = self.taskOrder[t_]
                 headId_ = self.headOrder[t_]
 
-                if self.accuracy[taskId_,t] == 0:
+                if self.accuracy[taskId_][t] == 0:
                     # overwrite qPosterior on q_pred
                     #qPred = deepcopy(self.qPosterior)
                     q_pred = ParametersDistribution(self.sharedWeightDim, self.headWeightDim, self.numHeads)
@@ -112,21 +112,28 @@ class VariationalTrainer:
 
                     if self.numHeads == 1:
                         if len(x_coresets) > 0:
-                            x_coreset, y_coreset = merge_coresets(x_coresets, y_coresets)
-                            q_pred.overwrite(maximizeVariationalLowerBound(q_pred, x_coreset, y_coreset, headId_))
+                            x_coreset, y_coreset = self.merge_coresets(x_coresets, y_coresets)
+                            q_pred.overwrite(self.maximizeVariationalLowerBound(q_pred, x_coreset, y_coreset, headId_))
                     else:
                         if len(x_coresets) > 0:
-                            q_pred.overwrite(maximizeVariationalLowerBound(q_pred, x_coresets[taskId_], y_coresets[taskId_], headId_))
+                            q_pred.overwrite(self.maximizeVariationalLowerBound(q_pred, x_coresets[taskId_], y_coresets[taskId_], headId_))
 
-                    parameters = q_pred.parameters()
-                    model = VanillaNN(self.inputDim, self.hiddenSize, self.numSharedlayers+self.numHeadLayers, self.outputDim)
+
+                    #parameters = q_pred.parameters()
+                    model = VanillaNN(self.inputDim, self.hiddenSize, self.numSharedLayers+self.numHeadLayers, self.outputDim)
                     monteCarlo = MonteCarlo(model)
+
                     y_pred = monteCarlo.computeMonteCarlo(x_testsets[taskId_], q_pred, headId_, self.numSamples)
                     _, y_pred = torch.max(y_pred.data, 1)
 
+                    print(y_pred.size()) #.data[0,:]
+                    print(y_testsets[taskId_][0,:])
+
+                    y_pred = torch.eye(10)[y_pred]
+
                     acc = torch.sum(torch.mul(y_pred, y_testsets[taskId_])) / y_pred.shape[0]
 
-                    self.accuracy[taskId_,t] = acc
+                    self.accuracy[taskId_][t] = acc
 
     def merge_coresets(self, x_coresets, y_coresets):
         x_coresets_list = list(x_coresets.values())
@@ -138,12 +145,12 @@ class VariationalTrainer:
     def getBatch(self, x_train, y_train):
         self.batchSize
         batches = []
-        numberOfBatches = x_train.size() / self.batchSize
+        numberOfBatches = x_train.shape[0] / self.batchSize
         if isinstance(numberOfBatches, int):
             errMessage = 'Batch size {} not consistent with dataset size {}' \
-                .format(x_train.size(), self.batchSize)
+                .format(x_train.shape[0], self.batchSize)
             raise Exception(errMessage)
-        for i in range(numberOfBatches):
+        for i in range(int(numberOfBatches)):
             start = i*self.batchSize
             end = (i+1)*self.batchSize
             x_train_batch = x_train[start:end]
@@ -154,23 +161,24 @@ class VariationalTrainer:
     def maximizeVariationalLowerBound(self, oldPosterior, x_train, y_train, headId):
         # create dummy new posterior
         newPosterior = ParametersDistribution(self.sharedWeightDim, self.headWeightDim, self.numHeads)
-        newPoterior.overwrite(oldPosterior)
+        newPosterior.overwrite(oldPosterior)
 
-        parameters = newPostrior.getFlattenedParameters(headId)
+        parameters = newPosterior.getFlattenedParameters(headId)
         optimizer = torch.optim.Adam(parameters, lr = 0.001)
 
-        for x_train_batch, y_train_batch in getBatch(x_train, y_train):
-            lossArgs = (x_train_batch, y_train_batch, newPosterior, oldPosterior, headId)
-            minimizeLoss(1000, optimizer, computeCost, lossArgs)
+        for x_train_batch, y_train_batch in self.getBatch(x_train, y_train):
+            model = VanillaNN(self.inputDim, self.hiddenSize, self.numSharedLayers+self.numHeadLayers, self.outputDim)
+            lossArgs = (model, x_train_batch, y_train_batch, newPosterior, oldPosterior, headId)
+            minimizeLoss(2, optimizer, computeCost, lossArgs) #1000
 
         return newPosterior
 
 # experiment setup
 dictParams = {
-'numEpochs':100,
+'numEpochs':1,
 'batchSize':10,
-'numSamples':10,
-'dataGen':PermutedMnistGen(),
+'numSamples':2,
+'dataGen':SplitMnistGen(),
 'numTasks':1,
 'numHeads':1,
 'coresetMethod':coreset_rand,
