ca0a65f4a2b6339c6c35936633481bbbada85b49
rb876 <rb876@cam.ac.uk>
2019-03-03 17:59:36 +0100

    coreset_k GPU friendly

--- src/coreset.py
+++ src/coreset.py
@@ -1,6 +1,6 @@
 
 import torch
-
+from constants import FloatTensor
 # Random Selection
 def coreset_rand(x_train, y_train, coreset_size):
     # randomly permute the indices
@@ -18,7 +18,7 @@ def coreset_rand(x_train, y_train, coreset_size):
 # K-center Selection
 def coreset_k(x_train, y_train, coreset_size):
     # distance tensor contains the distance to the furthest center, for each data point
-    distance = torch.ones(x_train.shape[0])*float('inf').type(FloatTensor)
+    distance = torch.ones(x_train.shape[0]).type(FloatTensor)*float('inf')
     # randomly select the first center
     cur_idx = torch.randint(x_train.shape[0], (1,)).item()
     # list idx will contain the indices of the coreset
