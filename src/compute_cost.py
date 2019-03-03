994979b6bad6a5df29557d245ab7f3bbabb0b78a
rb876 <rb876@cam.ac.uk>
2019-03-03 16:24:17 +0100

    + numsamples

--- src/compute_cost.py
+++ src/compute_cost.py
@@ -8,8 +8,9 @@ from constants import FloatTensor, MEAN, VARIANCE, WEIGHT, BIAS
 from monte_carlo import MonteCarlo
 from KL import KL
 
-def computeCost(inputs, labels, qPos, qPri, taskId):
-    monteCarlo = MonteCarlo(qPos, inputs.size()[0])
+
+def computeCost(inputs, labels, qPos, qPri, taskId, numSamples):
+    inputSize = inputs.size()[0]
+    monteCarlo = MonteCarlo(qPos, numSamples)
     kl = KL()
-    cost = - (monteCarlo.logPred(inputs, labels, taskId) - torch.div(kl.computeKL(qPos, qPri, taskId), inputs.size()[0]))
-    return  cost
+    return  - (monteCarlo.logPred(inputs, labels, taskId) - torch.div(kl.computeKL(qPos, qPri, taskId), inputSize))
