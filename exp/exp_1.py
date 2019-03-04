2f2c637666c339edcf33f4bd556553ee73285362
Gwangbin Bae <baegwangbin@gmail.com>
2019-03-04 00:31:46 +0000

    change batch size

--- exp/exp_1.py
+++ exp/exp_1.py
@@ -49,12 +49,14 @@ pickle.dump(accuracy, open( "results/PM_VCL_KC_200.p", "wb"))
 
 # 3. K-center Coreset only
 dictParams['coresetOnly'] = True
+dictParams['batchSize'] = 200
 trainer = VariationalTrainer(dictParams)
 accuracy = trainer.train()
 pickle.dump(accuracy, open( "results/PM_VCL_KCO_200.p", "wb"))
 
 # 4. VCL + Random Coreset
 dictParams['coresetOnly'] = False
+dictParams['batchSize'] = 256
 dictParams['coresetMethod'] = coreset_rand
 for size in [200, 400, 1000, 2500, 5000]:
     dictParams['coresetSize'] = size
@@ -64,6 +66,7 @@ for size in [200, 400, 1000, 2500, 5000]:
 
 # 5. Random Coreset only
 dictParams['coresetOnly'] = True
+dictParams['batchSize'] = 200
 for size in [200, 400, 1000, 2500, 5000]:
     dictParams['coresetSize'] = size
     trainer = VariationalTrainer(dictParams)
