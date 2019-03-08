2be15dabdeb5ac1d8d189cefabc04bccfd8dba16
unknown <justin_bunker@hotmail.com>
2019-03-08 16:05:47 +0000

    Modify setup for testing variational trainer

--- test/test_variational_trainer.py
+++ test/test_variational_trainer.py
@@ -6,26 +6,34 @@ from data_gen import *
 from coreset import *
 from variational_trainer import VariationalTrainer
 
-
-
-# experiment setup
+# default setup
 dictParams = {
-'numEpochs':1,
-'batchSize':100,
-'numSamples':100,
-'dataGen':PermutedMnistGen(),
-'numTasks':2,
-'numHeads':1,
-'coresetMethod':coreset_k,
-'coresetOnly': False, 
-'coresetSize':200,
-'numLayers':(2,1),
+'numEpochs':120,
+'alpha':1,
+'dataGen':SplitMnistGen(set0 = [0,2,4,6,8], set1 = [1,3,5,7,9]),
+'numTasks':5,
+'numHeads':5,
+'numLayers':(1,1),
 'hiddenSize':256,
-'taskOrder':[],
-'headOrder':[],
+'taskOrder':[0,1,2,3,4],
+'headOrder':[0,1,2,3,4],
 }
 
-# run experiment
+# Pickle file naming convention:
+# {0}_{1}_{2}_{3}.p
+# {0}: data gen (e.g. Permuted Mnist -> PM, Split Mnist -> SM, Permuted NotMnist = PN, Split NotMnist = SN)
+# {1}: method (e.g. VCL)
+# {2}: coreset (e.g. Random Coreset -> RC, Random Coreset Only -> RCO)
+# {3}: coreset size
+
+# 1. VCL (no Coreset)
+dictParams['batchSize'] = None
+dictParams['coresetSize'] = 0
+dictParams['coresetMethod'] = coreset_rand
+dictParams['coresetOnly'] = False
+
 trainer = VariationalTrainer(dictParams)
-trainer.train()
+accuracy = trainer.train()
+
+# run experiment
 print(trainer.accuracy)
