1018f31109a0353846ccd5a8b8cba2b4076170af
unknown <justin_bunker@hotmail.com>
2019-03-10 22:21:30 +0000

    Add fashion mnist

--- src/data_gen.py
+++ src/data_gen.py
@@ -10,6 +10,8 @@ from copy import deepcopy
 from scipy.io import loadmat
 from constants import FloatTensor
 
+torchvision.datasets.FashionMNIST(root='../data/fashion_mnist', train=True, download=True, transform=None)
+torchvision.datasets.FashionMNIST(root='../data/fashion_mnist', train=False, download=True, transform=None)
 
 # Mnist Data Loader
 class Mnist():
@@ -192,6 +194,54 @@ class PermutedNotMnistGen(NotMnist):
 
             next_x_test = deepcopy(self.X_test)[:,idx]
             next_y_test = torch.eye(10)[self.Y_test].type(FloatTensor)
+
+            self.curIter += 1
+
+            return next_x_train, next_y_train, next_x_test, next_y_test
+
+# FashionMnist Data Loader
+class FashionMnist():
+    def __init__(self):
+        x_train, y_train = torch.load('../data/fashion_mnist/processed/training.pt')
+        x_test, y_test = torch.load('../data/fashion_mnist/processed/test.pt')
+        length = x_train.shape[0]
+        self.X_train = x_train.view(x_train.shape[0], -1)
+        self.Y_train = y_train
+        self.X_test = x_test.view(x_test.shape[0], -1)
+        self.Y_test = y_test
+
+        plt.imshow(x_train[0].numpy().reshape(28,28), cmap = 'gray')
+
+# FashionMnist Generator
+class FashionMnistGen(FashionMnist):
+    # use the original order unless specified
+    def __init__(self, set0 = [0, 2, 4, 6, 8], set1 = [1, 3, 5, 7, 9]):
+        super().__init__()
+        self.maxIter = len(set0)
+        self.curIter = 0
+        self.set0 = set0
+        self.set1 = set1
+
+    def get_dims(self):
+        return self.X_train.shape[1], 2
+
+    def next_task(self):
+        if self.curIter >= self.maxIter:
+            raise Exception('Task finished!')
+        else:
+            train_id_0 = self.X_train[self.Y_train == self.set0[self.curIter], :]
+            train_id_1 = self.X_train[self.Y_train == self.set1[self.curIter], :]
+            next_x_train = torch.cat([train_id_0, train_id_1], dim=0).type(FloatTensor)
+
+            next_y_train = torch.cat([torch.ones(train_id_0.shape[0], 1),torch.zeros(train_id_1.shape[0], 1)],dim=0)
+            next_y_train = torch.cat([next_y_train, 1-next_y_train], dim=1).type(FloatTensor)
+
+            test_id_0 = self.X_test[self.Y_test == self.set0[self.curIter], :]
+            test_id_1 = self.X_test[self.Y_test == self.set1[self.curIter], :]
+            next_x_test = torch.cat([test_id_0, test_id_1], dim=0).type(FloatTensor)
+
+            next_y_test = torch.cat([torch.ones(test_id_0.shape[0], 1),torch.zeros(test_id_1.shape[0], 1)],dim=0)
+            next_y_test = torch.cat([next_y_test, 1-next_y_test], dim=1).type(FloatTensor)
 
             self.curIter += 1
 
