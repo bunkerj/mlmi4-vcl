4d1fd9d9fb9d59e91b2389befd611603cc21ca95
rb876 <rb876@cam.ac.uk>
2019-03-30 19:26:17 +0100

    new pipeline experiments file

---
+++ exp/new_exp_pipeline.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('../')
+sys.path.append('../src/')
+
+from data_gen import *
+from coreset import *
+from utils_exp import *
+from constants import DEFAULT_PARAMETERS
+
+dictUpdate = {'numLayers' : (1,2), 'coresetSize': 40'}
+dictParams = getAllExpParameters(dictUpdate)
+for taskOrder in getAdversarialPermutationList():
+    dictUpdate = {'taskOrder' : taskOrder}
+    dictParams = getAllExpParameters(dictUpdate)
+    for iter in range(0,5):
+        trainer = VariationalTrainer(dictParams)
+        accuracy = trainer.train()
