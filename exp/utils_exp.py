7680b7037f925d06c6506bdb72fa8574f49d1beb
rb876 <rb876@cam.ac.uk>
2019-03-30 19:53:58 +0100

    'stuff'

--- exp/utils_exp.py
+++ exp/utils_exp.py
@@ -1,15 +1,13 @@
 import sys
 sys.path.append('../')
 sys.path.append('../src/')
-
-from data_gen import *
-from coreset import *
+import itertools
 from copy import deepcopy
 from constants import DEFAULT_PARAMETERS
 
 def getAllExpParameters(updateObj, visUpdate = True):
 
-    updatedDictionary  = DEFAULT_PARAMETERS.deepcopy()
+    updatedDictionary  = deepcopy(DEFAULT_PARAMETERS)
     updatedDictionary.update(updateObj)
     if visUpdate:
         print("Experiments Settings : %s" %  updatedDictionary)
@@ -21,4 +19,7 @@ def getAdversarialPermutationList():
     baselineOrder = [1,2,3,4]
     allPermutations = [list(item) for item in itertools.permutations(baselineOrder)]
     allPermutations = [[0] + item for item in allPermutations]
-    pass allPermutations
+    return allPermutations
+
+def getName(dictionary, dictEntry):
+    return dictEntry+'_'+str(dictionary[dictEntry]).replace('[', '').replace(']', '').replace(', ', '_')
