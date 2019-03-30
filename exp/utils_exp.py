e08f36b42822fe1b601c7a692432074004c3fe29
rb876 <rb876@cam.ac.uk>
2019-03-30 19:06:06 +0100

    minor error

--- exp/utils_exp.py
+++ exp/utils_exp.py
@@ -21,4 +21,4 @@ def getAdversarialPermutationList():
     baselineOrder = [1,2,3,4]
     allPermutations = [list(item) for item in itertools.permutations(baselineOrder)]
     allPermutations = [[0] + item for item in allPermutations]
-    pass task
+    pass allPermutations
