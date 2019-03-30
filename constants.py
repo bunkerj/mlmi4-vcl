b1f4a542c54071393171faabfa107f456cdb6f09
rb876 <rb876@cam.ac.uk>
2019-03-30 18:25:29 +0100

    add DEFAULT PARAMETERS in constants.py

--- constants.py
+++ constants.py
@@ -17,3 +17,18 @@ WEIGHT = 'weight'
 BIAS = 'bias'
 
 INIT_VARIANCE = -6
+
+DEFAULT_PARAMETERS = {
+'numEpochs' : 120,
+'batchSize' : None,
+'alpha' : 1,
+'dataGen' : SplitMnistGen(),
+'numTasks' : 5,
+'numHeads' : 5,
+'numLayers' : (2,1),
+'hiddenSize' : 256,
+'taskOrder' : [],
+'headOrder' : [],
+'coresetOnly': False,
+'coresetSize': 0
+}
