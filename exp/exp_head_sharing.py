7b96f7d92b00c7dd85b5a686404f3a98a39e0cd9
unknown <justin_bunker@hotmail.com>
2019-03-31 18:21:08 +0100

    Add the head sharing experiment

---
+++ exp/exp_head_sharing.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append('../')
+sys.path.append('../src/')
+
+from time import time
+from data_gen import *
+from coreset import *
+from utils import *
+from constants import DEFAULT_PARAMETERS
+from result_averager import ResultAverager
+from variational_trainer import VariationalTrainer
+
+directory = "../exp/final_experiments/adversarial_ordering"
+
+dictUpdate = {
+    'dataGen':SplitMnistGen(),
+    'coresetMethod': coreset_rand,
+    'numLayers' : (2,1),
+    'coresetSize': 0,
+    'numEpochs' : 120,
+    'taskOrder': list(range(5))
+}
+
+for taskOrder in headOrderList():
+    startTime = time()
+    resultAverager = ResultAverager()
+    dictUpdate['taskOrder'] = taskOrder
+    dictParams = getAllExpParameters(dictUpdate)
+    for iter in range(0,5):
+        trainer = VariationalTrainer(dictParams)
+        accuracy = trainer.train()
+        resultAverager.add(accuracy)
+    filename = getName(dictParams, 'taskOrder')
+    writePerformanceRecordAccuracyAvg(directory, filename, resultAverager)
+    print('Time for single iteration: {}'.format((time() - startTime) / 60))
