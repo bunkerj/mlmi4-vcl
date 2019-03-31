7f4d86a22f615ed4f1ed127d7417550d834f5fc1
unknown <justin_bunker@hotmail.com>
2019-03-31 18:45:30 +0100

    Change scope of head sharing experiment

--- exp/exp_head_sharing.py
+++ exp/exp_head_sharing.py
@@ -17,19 +17,24 @@ dictUpdate = {
     'coresetMethod': coreset_rand,
     'numLayers' : (2,1),
     'coresetSize': 0,
-    'numEpochs' : 120,
+    'numEpochs' : 5,
     'taskOrder': list(range(5))
 }
 
-for headOrder in getHeadOrderList():
-    startTime = time()
-    resultAverager = ResultAverager()
-    dictUpdate['headOrder'] = headOrder
-    dictParams = getAllExpParameters(dictUpdate)
-    for iter in range(0,5):
-        trainer = VariationalTrainer(dictParams)
-        accuracy = trainer.train()
-        resultAverager.add(accuracy)
-    filename = getName(dictParams, 'headOrder')
-    writePerformanceRecordAccuracyAvg(directory, filename, resultAverager)
-    print('Time for single iteration: {}'.format((time() - startTime) / 60))
+headCounts = [1,2]
+taskCount = 5
+
+for headCount in headCounts:
+    for headOrder in getHeadOrderList(headCount, taskCount):
+        startTime = time()
+        resultAverager = ResultAverager()
+        dictUpdate['headOrder'] = headOrder
+        dictParams = getAllExpParameters(dictUpdate)
+        for iter in range(0,5):
+            trainer = VariationalTrainer(dictParams)
+            accuracy = trainer.train()
+            resultAverager.add(accuracy)
+        filename = '{}_headcount_{}'.format(getName(dictParams, 'headOrder'), headCount)
+        writePerformanceRecordAccuracyAvg(directory, filename, resultAverager)
+        print('Time for single iteration: {}'.format((time() - startTime) / 60))
+        break
