93718f3863b558f36a111da8241765033348013f
unknown <justin_bunker@hotmail.com>
2019-04-02 11:11:08 +0100

    Add adversarial_ordering_1_2 experiments

---
+++ exp/exp_adversarial_ordering_1_2.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append('../')
+sys.path.append('../src/')
+
+from time import time
+from data_gen import *
+from coreset import *
+from utils import *
+from constants import DEFAULT_PARAMETERS
+from result_averager import ResultAverager
+from variational_trainer import VariationalTrainer
+
+directory = "../exp/final_experiments/adversarial_ordering_1_2"
+
+dictUpdate = {
+    'dataGen':SplitMnistGen(),
+    'coresetMethod': coreset_rand,
+    'numLayers' : (1,2),
+    'coresetSize': 0,
+    'numEpochs' : 120
+}
+
+for taskOrder in getAdversarialPermutationList():
+    startTime = time()
+    resultAverager = ResultAverager()
+    dictUpdate['taskOrder'] = taskOrder
+    dictParams = getAllExpParameters(dictUpdate)
+    for iter in range(0,5):
+        trainer = VariationalTrainer(dictParams)
+        accuracy = trainer.train()
+        resultAverager.add(accuracy)
+    filename = getName(dictParams, 'taskOrder')
+    writePerformanceRecordAccuracyAvg(directory, filename, resultAverager)
+    print('Time for single iteration: {}'.format((time() - startTime) / 60))
